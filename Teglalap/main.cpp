40b5ad186d785af36d63b0da929af6d249d584ce
Army <armyvagyok@gmail.com>
2020-09-08 10:27:00 +0200

    update

---
+++ Teglalap/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std; //névtér
+
+int main()
+{
+    //std::cout << "Hello world!" << endl; ha nem használunk névteret.
+    cout << "Hello world!" << endl;
+    return 0;
+}
