b80dc3f040521a84a95ed13bacc75ba5f70cd142
Army <armyvagyok@gmail.com>
2020-09-15 10:32:04 +0200

    update

---
+++ 2020_09_15_2ora/LNKO/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //Ékezetes betûk
+    setlocale(LC_ALL,"hun");
+    //Hibakezeléshez
+    bool hiba;
+    //bemenet
+    int a,b;
+    //Kimenet
+    int LNKO;
+    //beolvasás
+    cout << "Kiszámoljuk két szám legnagyobb közös osztóját." << endl;
+
+
+
+
+    return 0;
+}
